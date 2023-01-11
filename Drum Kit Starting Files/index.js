ffc782db969545710fa9fb8f79cc67d24287b621
hoangr1010 <huyhoangr1010@gmail.com>
2023-01-11 16:15:28 -0700

    update

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,88 @@
+
+const makeallert = () => {
+    alert('hehe')
+}
+
+
+for (i=0; i<document.querySelectorAll('.drum').length; i++) {
+    document.querySelectorAll('.drum')[i].addEventListener('click', function () {
+        switch (this.innerHTML) {
+            case 'w':
+                var audio = new Audio('sounds/crash.mp3');
+                audio.play();
+                break;
+            case 'a':
+                var audio = new Audio('sounds/kick-bass.mp3');
+                audio.play();
+                break;
+            case 's':
+                var audio = new Audio('sounds/snare.mp3');
+                audio.play();
+                break;   
+            case 'd':
+                var audio = new Audio('sounds/tom-1.mp3');
+                audio.play();
+                break;       
+            case 'j':
+                var audio = new Audio('sounds/tom-2.mp3');
+                audio.play();
+                break;
+            case 'k':
+                var audio = new Audio('sounds/tom-3.mp3');
+                audio.play();
+                break;   
+            case 'l':
+                var audio = new Audio('sounds/tom-4.mp3');
+                audio.play();
+                break;     
+        };
+
+        buttonAnimation(this.innerHTML)
+    }      
+)}
+
+document.addEventListener('keypress', function(event) {
+    switch (event.key) {
+        case 'w':
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+            break;
+        case 'a':
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+            break;
+        case 's':
+            var audio = new Audio('sounds/snare.mp3');
+            audio.play();
+            break;   
+        case 'd':
+            var audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+            break;       
+        case 'j':
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case 'k':
+            var audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+            break;   
+        case 'l':
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+            break;     
+    };
+
+    buttonAnimation(event.key)
+})
+
+const buttonAnimation = (key) => {
+    document.querySelector(`.${key}`).classList.add('press');
+    
+    setTimeout(function() {
+    
+        document.querySelector(`.${key}`).classList.remove('press');
+
+    }, 100)
+    
+}
