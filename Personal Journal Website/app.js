d2f86c98503b200f0c7e078f5669b2377a5281b8
hoangr1010 <huyhoangr1010@gmail.com>
2023-01-23 20:39:53 -0700

    Journal update MongoDB

--- Personal Journal Website/app.js
+++ Personal Journal Website/app.js
@@ -4,30 +4,64 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ =require('lodash');
+const mongoose = require('mongoose');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+//connect to MongoDB
+mongoose.set('strictQuery', false);
+mongoose.connect("mongodb://localhost:27017/journalDB", { useNewUrlParser: true});
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const app = express();
+//Create new Schema for journal
+const journalSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    maxLength: 255
+  }, 
+  body: {
+    type: String,
+    required: true
+  }
+})
 
-app.set('view engine', 'ejs');
+// Create new colletion for journal
+const Journal = mongoose.model('Journal', journalSchema);
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+// //test mongodb
+// const test = new Journal({
+//   name: 'test',
+//   body: 'tahsdhvhahsdvhhashdhvhahsdhv'
+// })
+// test.save()
 
 // initialize list of post and list of title name
 var contentList = []
 
-var titleList = contentList.map((content) => content.title)
 
 app.get('/', function (req, res) {
-  res.render('home', {
-    headingContent: homeStartingContent,
-    contentList: contentList
+  Journal.find(function (err, journalArray) {
+    if (err) {
+      console.error(err)
+    } else {
+      res.render('home', {
+        headingContent: homeStartingContent,
+        contentList: journalArray
+      })
+    }
   })
 })
 
+
+// default info --------------------------------------------------
 app.get('/about', function (req, res) {
   res.render('about', {aboutContent: aboutContent})
 })
@@ -39,45 +73,49 @@ app.get('/contact', function (req, res) {
 app.get('/compose', function (req, res) {
   res.render('compose')
 })
-
+//----------------------------------------------------------------
 
 app.post('/compose', function (req, res) {
   //parse request to content
   let title = req.body.title;
   let body = req.body.postBody;
-  var content = {
-    title: title,
-    body: body
-  }
-  
-  // save to main list
-  contentList.push(content)
-  console.log(contentList)
+
+  // add to journalDB
+  addToDB(title, body)
 
   //direct to home
   res.redirect('/')
 })
 
-app.get('/posts/:topic', (req, res) => {
+app.get('/posts/:topicId', (req, res) => {
   //get topic from path route
-  var titleList = contentList.map((content) => _.lowerCase(content.title))
-  var topic = _.lowerCase(req.params.topic)
-  // show topic found or not
-  if (titleList.includes(topic)) {
-    // show result
-    console.log('found topic')
-
-    // pass content to post
-    let index = titleList.indexOf(topic)
-    let titlePage = contentList[index].title
-    let contentPage = contentList[index].body
-    res.render('post', {titlePage: titlePage, bodyPage: contentPage})
-
-  } else {
-    console.log('not found topic')
-  }
+  var topicId = req.params.topicId
+  //search
+  query = {_id: topicId}
+  Journal.findOne(query, function (err, journal) {
+    if (err) {
+      console.log(err)
+    } else {
+      res.render('post', {titlePage: journal.name, bodyPage: journal.body})
+    }
+  })
 })
+
+
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
+
+
+//---------------------------------------------------------------- functionality ----------------------------------------------------------------
+
+function addToDB(title, body) {
+  var content = new Journal({
+    name: title,
+    body: body
+  })
+  content.save()
+  console.log('new journal added to database')
+}
