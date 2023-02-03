140f4cd02a05c95a6bd48ec69a4561336929f38c
hoangr1010 <huyhoangr1010@gmail.com>
2023-02-02 20:48:02 -0700

    update Keeper with React

--- Keeper (with React)/src/components/Box.jsx
+++ Keeper (with React)/src/components/Box.jsx
@@ -1,8 +1,15 @@
 import React, { useState } from 'react';
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
+
+
+
 
 function Box(props) {
 
     const [note, changeNote] = useState({title: '',content: ''})
+    const [isZoom, changeZoom] = useState(false)
 
     function updateNote(event) {
         var {name, value} = event.target
@@ -15,18 +22,24 @@ function Box(props) {
         })
     }
 
+    function openBox() {
+        changeZoom(true)
+    }
+
     return (
         <div>
-            <form>
-                <input onChange={updateNote} name="title" placeholder="Title" value={note.title} />
-                <textarea onChange={updateNote} name="content" placeholder="Take a note..." rows="3" value={note.content} />
-                <button 
-                    type='button' 
-                    onClick={() => {
-                        props.onClick(note)
-                        changeNote({title: '',content: ''}) 
-                        }}
-                >Add</button>            
+            <form className="create-note">
+                <input style={{display: isZoom ? 'inline-block' : 'none'}} onChange={updateNote} name="title" placeholder="Title" value={note.title} />
+                <textarea onClick={isZoom ? 'none' : openBox} onChange={updateNote} name="content" placeholder="Take a note..." rows={isZoom ? '3' : '1' } value={note.content} />
+                    <Zoom in={isZoom}>
+                        <Fab
+                            type='button' 
+                            onClick={() => {
+                                props.onClick(note)
+                                changeNote({title: '',content: ''}) 
+                                }}
+                        ><AddIcon /></Fab>   
+                    </Zoom>         
             </form>
         </div>
     )
--- Keeper (with React)/src/components/Note.jsx
+++ Keeper (with React)/src/components/Note.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function Note(props) {
 
@@ -6,9 +7,9 @@ function Note(props) {
     return <div className='note'>
         <h1>{props.title}</h1>
         <p>{props.content}</p>
-        <button 
-            onClick={() => props.delete((props.id))}
-        >DELETE</button>
+        <button onClick={() => props.delete((props.id))}>
+            <DeleteIcon />
+        </button>
     </div>
 }
 
